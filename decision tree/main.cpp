6b24701da0078883983268dfa1cdec30476facc9
XuXo <tianxing.xu@gmail.com>
2014-02-01 00:56:02 -0500

    Initial Commit

---
+++ decision tree/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  decision tree
+//
+//  Created by XuXo on 2/1/14.
+//  Copyright (c) 2014 XuXo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
